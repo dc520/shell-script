5f9f9b11d0e87c7d5a2609af0cd50c6f85bd0b1f
dc520 <359594776@qq.com>
2017-08-24 17:02:04 +0800

    添加日志收集

---
+++ all-logs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+/data/managelog/nginx_access.sh mall-ssl 阿拉小优日志
+/data/managelog/nginx_access.sh tmh-ssl 童萌惠日志
+/data/managelog/nginx_access.sh wxyx 微信营销日志
+/data/managelog/nginx_access.sh crm-ssl CRM日志
+/data/managelog/nginx_access.sh shequ-wechat-ssl 社区旧前端日志
+/data/managelog/nginx_shequ_access.sh shequ-nginx 社区nginx日志
